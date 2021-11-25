1e9edc9eec910309ad66f3d7528e4b4874526f08
Phillip Gomez <fillet54@gmail.com>
2021-11-25 04:07:37 +0000

    Only construct application once

--- backend/app.py
+++ backend/app.py
@@ -5,25 +5,29 @@ from middleware import config_middleware, with_middleware
 from utils import not_found_handler
 from image import image_handler
 
+def make_app():
 
-@with_middleware([
-    config_middleware({
-        'IMAGE_ROOT': '/opt/site/images',
-        'CACHE_ROOT': '/opt/site/cache'})
-])
-def application(environ, start_response):
     routes = [
         GET('/image/*', image_handler)
     ]
-    for route in routes:
-        handler = route(environ)
-        if handler is not None:
-            response = handler(environ, start_response)
-            if response:
-                return response
 
-    return not_found_handler(environ, start_response)
+    @with_middleware([
+        config_middleware({
+            'IMAGE_ROOT': '/opt/site/images',
+            'CACHE_ROOT': '/opt/site/cache'})
+    ])
+    def app(environ, start_response):
+        for route in routes:
+            handler = route(environ)
+            if handler is not None:
+                response = handler(environ, start_response)
+                if response:
+                    return response
+    
+        return not_found_handler(environ, start_response)
+    return app
 
+application = make_app()
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
